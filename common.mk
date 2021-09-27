e6b78dc970aec9a823708ba2489f1e593cabcdef
Andrei Rabusov <a.rabusov@tum.de>
2021-09-27 14:13:08 +0200

    Add *.ini for MiG-17 and the Earth, test constr

--- common.mk
+++ common.mk
@@ -1,6 +1,7 @@
 AM_CXXFLAGS =  -Wall -Wextra -Wpedantic -Werror -Wpedantic -Wshadow
 AM_CXXFLAGS += -Wfatal-errors
 AM_CXXFLAGS += -fanalyzer # Only for gcc 10
+AM_CXXFLAGS += -isystem $(shell pkg-config eigen3 --variable=prefix)/include/eigen3
 AM_CPPFLAGS = -DDATADIR='"$(pkgdatadir)"'
 AM_CRAZY_MATH = -O3 -march=native -mtune=native
 AM_CRAZY_MATH += -funroll-loops
